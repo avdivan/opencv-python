6999ffbcdedc806c4568dcf9a873b79d8ffd1625
Ivan Avdeev <avdeeff@internet.ru>
2025-07-30 15:09:49 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,5 +15,4 @@ echo $ENABLE_CONTRIB > contrib.enabled
 echo $ENABLE_HEADLESS > headless.enabled
 echo $ENABLE_ROLLING > rolling.enabled
 set -x
-export CMAKE_ARGS="-DWITH_QT=OFF"
 build_wheel $REPO_DIR $PLAT
